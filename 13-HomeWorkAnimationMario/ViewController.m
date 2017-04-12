0dff18be65cca7248e23189d9f214ffd9e0b713e
Slava <rtootik@gmail.com>
2017-04-12 22:52:46 +0300

    end

--- 13-HomeWorkAnimationMario/ViewController.m
+++ 13-HomeWorkAnimationMario/ViewController.m
@@ -9,11 +9,11 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) NSMutableArray *marioMutableArray;
+@property (weak, nonatomic) UIImageView *imgCloud;
+@property (weak, nonatomic) UIImageView *imgEarth;
 
-@property (strong, nonatomic) NSMutableArray *arrayWithRock;    // массив с камнями
-@property (strong, nonatomic) NSMutableArray *arrayWithCloud;   // массив с облачками
-@property (strong, nonatomic) UIView *earth;
-@property (strong, nonatomic) UIView *borderForCercle;
+
 
 @end
 
@@ -21,10 +21,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.arrayWithRock = [NSMutableArray new];
-    self.arrayWithCloud = [NSMutableArray new];
+    [self createCloud];
+    [self createMario];
+    [self createRect];
+
     
-//    self.view.backgroundColor = [UIColor blueColor];
     // создаем градиент неба
     CAGradientLayer *gradientSky = [CAGradientLayer new];
     gradientSky.frame = self.view.bounds;
@@ -33,90 +34,75 @@
     gradientSky.colors = @[(id)[UIColor colorWithRed:119.0 / 255.0 green:171.0 / 255.0 blue:255.0 / 255.0  alpha:1].CGColor,
                            (id)[UIColor colorWithRed:33 / 255.0 green:106 / 255.0 blue:224 / 255.0 alpha:1].CGColor];
     [self.view.layer insertSublayer:gradientSky atIndex:0];
-    
-    // границы для облаков
-    for (int i = 0; i < 8; i++) {
-        self.borderForCercle = [UIView new];
-        CGFloat randomX =+ 100;
-        CGFloat rangomY =+ 100;
-        self.borderForCercle = [self createRect:[UIColor colorWithWhite:0 alpha:0] withX:randomX withY:rangomY withWidth:arc4random_uniform(50) + 100 withHight:arc4random_uniform(50) + 50];
-        CGRect rectBorderForCercle = self.borderForCercle.bounds;
-        [self.view addSubview:self.borderForCercle];
-        // create Cloud
-        // подвижная
-        for (int j = 0; j < 1; j++) {
-            CGFloat randomX = arc4random_uniform(CGRectGetWidth(rectBorderForCercle));
-            CGFloat rangomY = arc4random_uniform(CGRectGetHeight(rectBorderForCercle));
-            UIView *circle = [UIView new];
-            circle = [self createRect:[UIColor redColor] withX:randomX withY:rangomY withWidth:50 withHight:50];
-            circle.layer.cornerRadius = 25;
-            [self.borderForCercle addSubview:circle];
-        }
-    }
-    
 
-//    UIView *circle = [UIView new];
-//    circle = [self createRect:[UIColor redColor] withX:55 withY:222 withWidth:50 withHight:50];
-//    circle.layer.cornerRadius = 25;
-//    [self.view addSubview:circle];
-//    [self moveCloud:circle];
-    
-    // create earth
-    CGRect rectEarth = self.view.bounds;
-    self.earth = [UIView new];
-    self.earth = [self createRect:[UIColor brownColor] withX:0 withY:0 withWidth:CGRectGetWidth(rectEarth) withHight:60];
-    [self.view addSubview:self.earth];
-    
-    //create shadow
-    CGRect rectShadow = self.view.bounds;
-    UIView *shadow = [UIView new];
-    shadow = [self createRect:[UIColor colorWithRed:0 green:0 blue:0 alpha:0.6f] withX:0 withY:55 withWidth:CGRectGetWidth(rectShadow) withHight:5];
-    [self.earth addSubview:shadow];
-    
-    // create grass
-    CGRect rectGrass = self.view.bounds;
-    UIView *grass = [UIView new];
-    grass = [self createRect:[UIColor greenColor] withX:0 withY:60 withWidth:CGRectGetWidth(rectGrass) withHight:10];
-    [self.earth addSubview:grass];
-    
-    //createRock
-        // подвижная
-    for (int i = 0; i < 12; i++) {
-        CGFloat randomX = arc4random_uniform(CGRectGetWidth(rectEarth));
-        CGFloat randomY = arc4random_uniform(50) + 1;
-        CGFloat randomWidth = arc4random_uniform(4) + 2;
-        CGFloat randomHight = arc4random_uniform(2) + 2;
-        UIView *rock = [UIView new];
-        rock = [self createRect:[UIColor blackColor] withX:randomX withY:randomY withWidth:randomWidth withHight:randomHight];
-        [self.earth addSubview:rock];
-        [self.arrayWithRock addObject:rock];
-    }
-    for (UIView *myRock in self.arrayWithRock) {
-        [self moveRock:myRock];
-    }
 
 }
-
-// метод создания квадратов (полей)
-- (UIView *) createRect:(UIColor *) colorRect withX:(CGFloat) x withY:(CGFloat) y withWidth:(CGFloat) width withHight:(CGFloat) hight {
-    CGRect rect = CGRectMake(x, y, width, hight);
-    UIView *createRect = [[UIView alloc] initWithFrame:rect];
-    createRect.backgroundColor = colorRect;
-//    [self.view addSubview:createRect];
-    return createRect;
+// create plaer
+- (void) createMario {
+    self.marioMutableArray = [NSMutableArray new];
+    // создаем массив с изображениями
+    CGFloat x = (CGRectGetHeight(self.view.bounds) / 2) + 100;
+    CGFloat y = CGRectGetMinY(self.view.bounds) + 35;
+    UIImageView *marioView = [[UIImageView alloc] initWithFrame:CGRectMake(x, y, 100, 100)];
+    [self.view addSubview:marioView];
+    NSInteger nameOfImg = 11;
+    do {
+        NSString *imgName = [NSString stringWithFormat:@"%d.png", nameOfImg--];   // добовляем img в массив
+        UIImage *marioImg = [UIImage imageNamed:imgName];
+        [self.marioMutableArray addObject:marioImg];
+    } while (nameOfImg > 0);
+    marioView.animationImages = self.marioMutableArray;
+    marioView.transform = CGAffineTransformMakeRotation(M_PI);  // какого-то хера оно перевернуло мне мзображение
+    marioView.animationDuration = .8;
+    [marioView startAnimating]; // анимируем img
+    
 }
 
+- (void) createCloud {
+    // создаем облака
+    CGFloat randCloudWidth = arc4random_uniform(100) + 50;
+    CGFloat randCloudHeight = arc4random_uniform(40) + 50;
+    CGFloat randX = CGRectGetMinX(self.view.bounds);
+    CGFloat randY = arc4random_uniform(CGRectGetMidY(self.view.bounds));
+    UIImageView *cloudImgView = [[UIImageView alloc] initWithFrame:CGRectMake(randX, randY, randCloudWidth, randCloudHeight)];
+    [self.view addSubview:cloudImgView];
+    UIImage *cloudImg = [UIImage imageNamed:@"cloud2.png"];
+    NSArray *arrayCloud = @[cloudImg];
+    cloudImgView.animationImages = arrayCloud;
+    cloudImgView.transform = CGAffineTransformMakeRotation(M_PI);   // какого-то хера оно перевернуло мне мзображение
+    [cloudImgView startAnimating];
+    self.imgCloud = cloudImgView;
+    [self moveCloud:self.imgCloud];
+}
 
-// метод анимации облаков
-- (void) moveRock:(UIView *) view  {
-    CGFloat x = CGRectGetMinX(view.frame);
-    CGFloat y = CGRectGetMinY(view.frame);
-
-
+- (void) createRect {
+    // создаем землю
+    CGFloat x = CGRectGetMinX(self.view.frame) - 300;
+    CGFloat y = CGRectGetMinY(self.view.frame);
+    CGFloat rectSize = CGRectGetHeight(self.view.frame) / 8;
+    for (int i = 0; i < self.view.bounds.size.height; i++) {
+        UIImageView *earthImgView = [[UIImageView alloc] initWithFrame:CGRectMake(x, y, rectSize, rectSize)];
+        [self.view addSubview:earthImgView];
+        UIImage *earthImg = [UIImage imageNamed:@"rock"];
+        NSArray *arrayRock = @[earthImg];
+        earthImgView.animationImages = arrayRock;
+        earthImgView.transform = CGAffineTransformMakeRotation(M_PI);   // какого-то хера оно перевернуло мне мзображение
+        [earthImgView startAnimating];
+        x += 30;
+        [self moveRock:earthImgView];
+    }
     
-    [UIView animateWithDuration:2
+}
+
+// метод анимации камня
+- (void) moveRock:(UIView *) view {
+//    CGFloat x = CGRectGetMinX(view.frame);
+//    CGFloat y = CGRectGetMinY(view.frame);
+    CGFloat x = CGRectGetMinX(view.frame) + 80;
+    CGFloat y = 20;
+    [UIView animateWithDuration:1
                           delay:0
-                        options:UIViewAnimationOptionCurveLinear
+                        options:UIViewAnimationOptionCurveLinear | UIViewAnimationOptionRepeat
                      animations:^{
                          view.center = CGPointMake(x, y);
                      } completion:^(BOOL finished) {
@@ -126,7 +112,33 @@
                      }];
 }
 
-// метод анимации камней
+// метод анимации облаков
+- (void) moveCloud:(UIView *) cloudView {
+//    CGFloat x = CGRectGetMaxX(cloudView.frame);
+//    CGFloat y = CGRectGetWidth(cloudView.frame);
+//    [UIView animateWithDuration:5
+//                          delay:0
+//                        options:UIViewAnimationOptionCurveLinear
+//                     animations:^{
+//                         cloudView.center = CGPointMake(x, y);
+//                     } completion:^(BOOL finished) {
+//                         NSLog(@"end");
+//                         __weak UIView *v = cloudView;
+//                         [self moveRock:v];
+//                     }];
+CGFloat x = CGRectGetMaxX(self.view.bounds) - CGRectGetWidth(cloudView.bounds);
+CGFloat y = CGRectGetMidY(cloudView.frame);
+[UIView animateWithDuration:10
+                      delay:0
+                    options:UIViewAnimationOptionCurveLinear
+                 animations:^{
+                     cloudView.center = CGPointMake(x, y);
+                 } completion:^(BOOL finished) {
+                     NSLog(@"end");
+                     __weak UIView *v = cloudView;
+                     [self moveRock:v];
+                 }];
+}
 
 
 
